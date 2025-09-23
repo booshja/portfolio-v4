2665c6e74623d8267ead83ed8181f3b447e8d70c
Jacob Andes <jacob.andes@gmail.com>
2025-09-22 20:09:16 -0700

    test: adjust mock unstable_flag to return async function to allow spying

--- src/testing/__mocks__/vercelFlagsNext.ts
+++ src/testing/__mocks__/vercelFlagsNext.ts
@@ -1,7 +1,12 @@
-export const unstable_flag = <T>(config: { key: string; defaultValue?: T; decide?: () => Promise<T> | T }) => {
-    return {
-        key: config.key,
-        get: async () => (typeof config.decide === "function" ? await config.decide() : (config.defaultValue as T)),
-    } as unknown as T;
+export const unstable_flag = <T>(config: {
+    key: string;
+    defaultValue?: T;
+    decide?: () => Promise<T> | T;
+}) => {
+    const fn = async () =>
+        typeof config.decide === "function"
+            ? await config.decide()
+            : (config.defaultValue as T);
+    (fn as unknown as { key: string }).key = config.key;
+    return fn as unknown as () => Promise<T>;
 };
-
