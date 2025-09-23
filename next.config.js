ce1826caa5d9427b8f67f25a8da79e8421f9c993
Jacob Andes <jacob.andes@gmail.com>
2025-09-22 22:18:09 -0700

    fix: use new @vercel/flags next export; remove deprecated instrumentationHook

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
-    experimental: {
-        instrumentationHook: true,
-    },
     images: {
         domains: [],
     },
